601dbd7f672141d2f1590fd040ffda41146817dd
Yeabserachi <yeabseraberhanu248@gmail.com>
2022-06-05 07:15:20 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,8 @@
-if the number is less than 0: is negative
+#include <stdlib.h>
+#include <time.h>
+/*The number, followed by*/
+if (n>0) // positive
+	if (n=0) // Zero
+	    if (n<0) // negative
+
+	
