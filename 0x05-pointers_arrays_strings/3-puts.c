b3c0a545aa76b1b06a7f89e1bcef228ff10d37fe
Yeabserachi <yeabseraberhanu248@gmail.com>
2022-06-14 13:21:18 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_puts - prints a string
+*0str: A pointer to an int that will be changed
+*
+*Return: void which means our answer is correct
+*/
+
+void _outs(char *str)
+{
+char *C;
+int _
