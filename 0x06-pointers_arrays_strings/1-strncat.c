1c38aa2e189182f83e5da33001579bda891971e4
Yeabserachi <yeabseraberhanu248@gmail.com>
2022-06-15 09:43:54 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncat - like _strcat but it will use n bytes from src
+ *
+ * @dest: string to be concatenated
+ * @src: string to be concatenated until n limit
+ * @n: number of bytes that will be used from src
+ *
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int x, y;
+
+c = 0;
+
+  /*find size of dest array*/
+while (dest[c])
+c++;
+
+  /**
+   * src does not need to be null terminated
+   * if it contains n or more bytes
+   */
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[c + i] = src[i];
+/*null terminate dest*/
+dest[c + i] = '\0';
+
+return (dest);
+}
